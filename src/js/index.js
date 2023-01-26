e83422c159f47e08ac42b16555868d48fe6f9d5c
pulgazion <reimarrodri@gmail.com>
2023-01-26 16:03:59 +0000

    Proyecto de contador con React

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,50 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+import PropTypes from "prop-types";
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
+        <i className="far fa-clock"></i>
+      </div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
+    </div>
+  );
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
+};
+
+let counter = 0;
+
+ReactDOM.render(
+  <SimpleCounter
+    digitOne={Math.floor(counter / 1)}
+    digitTwo={Math.floor(counter / 10)}
+    digitThree={Math.floor(counter / 100)}
+    digitFour={Math.floor(counter / 1000)}
+  />,
+  document.querySelector("#app")
+);
+
+setInterval(() => {
+  counter++;
+  ReactDOM.render(
+    <SimpleCounter
+      digitOne={Math.floor(counter / 1)}
+      digitTwo={Math.floor(counter / 10)}
+      digitThree={Math.floor(counter / 100)}
+      digitFour={Math.floor(counter / 1000)}
+    />,
+    document.querySelector("#app")
+  );
+}, 1000);
